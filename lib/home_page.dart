df13f7a46f65b6d9e99547c21e89ecb2da47139c
Prateek Patel <e21cseu0489@bennett.edu.in>
2023-07-21 02:15:27 +0530

    First Commit

---
+++ lib/home_page.dart
@@ -0,0 +1,134 @@
+import 'package:allen/feature_box.dart';
+import 'package:allen/pallete.dart';
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:  AppBar(
+        title: const Text('Allen'),
+        leading: const Icon(Icons.menu),
+        centerTitle: true,
+    ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // virtual assistant picture
+            Stack(
+              children: [
+                Center(
+                  child: Container(
+                    height: 120,
+                    width: 120,
+                    margin: const EdgeInsets.only(top:4),
+                    decoration:  const BoxDecoration(
+                      color: Pallete.assistantCircleColor,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 123,
+                  decoration:  const BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                    image: AssetImage(
+                        'assets/images/virtualAssistant.png',
+                    ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            // chat bubble
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 10,
+              ),
+              margin: const EdgeInsets.symmetric(horizontal: 40).copyWith(
+                top:30,
+              ),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Pallete.borderColor,
+                ),
+                borderRadius: BorderRadius.circular(20).copyWith(
+                  topLeft: Radius.zero,
+                ),
+              ),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0),
+                child: Text(
+                  'Good Morning, what task can I do for you?',
+                  style: TextStyle(
+                    fontFamily: 'Cera Pro',
+                    color: Pallete.mainFontColor,
+                    fontSize: 25,
+                  ),
+                ),
+              ),
+            ),
+           Container(
+             padding: const EdgeInsets.all(10),
+              alignment: Alignment.centerLeft,
+              child: const Text(
+                'Here are a few features',
+                style: TextStyle(
+                  fontFamily: 'Cera Pro',
+                  color: Pallete.mainFontColor,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            // features list
+            const Column(
+              children: [
+                FeatureBox(
+                    color: Pallete.firstSuggestionBoxColor,
+                    headerText: 'ChatGPT',
+                  descriptionText:
+                  'A smarter way to stay organized and informed with ChatGPT',
+                ),
+                FeatureBox(
+                  color: Pallete.secondSuggestionBoxColor,
+                  headerText: 'Dall-E',
+                  descriptionText:
+                  'Get inspired and stay creative with your personal assistant powered by Dall-E',
+                ),
+                FeatureBox(
+                  color: Pallete.thirdSuggestionBoxColor,
+                  headerText: 'Smart Voice Assistant',
+                  descriptionText:
+                  'Get the best of both worlds with a voice assistant powered by Dall-E and ChatGPT',
+                ),
+
+              ],
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Pallete.firstSuggestionBoxColor,
+        onPressed: () {},
+        child: const Icon(Icons.mic),
+      ),
+
+    );
+  }
+}
