4dd109a03d30e3e3d603cecd606a891f49502b39
Prateek Patel <e21cseu0489@bennett.edu.in>
2023-07-27 08:10:45 +0530

    updating

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Allen',
+      title: 'Lyra',
       theme: ThemeData.light(useMaterial3: true).copyWith(
         scaffoldBackgroundColor: Pallete.whiteColor,
         appBarTheme: const AppBarTheme(
